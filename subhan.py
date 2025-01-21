272dde75c0c60a6a679f2e01ec0e9c4af87b2f79
subhansmd6 <subhansmd6@gmail.com>
2025-01-21 16:34:30 +0530

    5 new files added

---
+++ subhan.py
@@ -0,0 +1,6 @@
+this is python file
+1st line in text file[D[D[D[python file
+1st line in text file
+1st line in text file
+1st line in text file
+1st line in text file[A[A[A[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D2nd3rd4th5th[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C
